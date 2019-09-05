372c74f7382b5b2259cf5026516c3e6b1ab7de27
MagicBoomliu <Luke-Liu2020@qq.com>
2019-09-05 15:27:53 +0800

    day3
    change something

--- Day2_20190903/Object_detections/Google_API_and_cv2.py
+++ Day2_20190903/Object_detections/Google_API_and_cv2.py
@@ -20,7 +20,7 @@ NUM_CLASSES = 90
 import cv2
 
 cap = cv2.VideoCapture(0)
-
+# 加载图模型
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -28,11 +28,13 @@ with detection_graph.as_default():
         od_graph_def.ParseFromString(fid.read())
         tf.import_graph_def(od_graph_def, name='')
 
+#加载类别，加载label
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
                                                             use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
+#记载课时使用到的输出与输入tensor
 with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
         image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -45,7 +47,7 @@ with detection_graph.as_default():
             ret, image_np = cap.read()
             image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
             image_np_expanded = np.expand_dims(image_np, axis=0)
-
+# 得到参数，类别以及确信度。
             (boxes, scores, classes, num) = sess.run(
                 [detection_boxes, detection_scores, detection_classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
@@ -54,7 +56,6 @@ with detection_graph.as_default():
                                                                np.squeeze(classes).astype(np.int32), np.squeeze(scores),
                                                                category_index, use_normalized_coordinates=True,
                                                                line_thickness=8)
-
 
             cv2.imshow('object detection', cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
             if cv2.waitKey(25) & 0xFF == ord('q'):
